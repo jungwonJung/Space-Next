ef0c379658d045a5487596abbad29d954386d2e1
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-05-17 16:34:59 +0200

    test

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -2,6 +2,7 @@ const API_URL = "https://fdo.rocketlaunch.live/json/launches/next/5";
 const countdown = document.getElementById("countdown");
 
 //noice work jay, i am proud of you
+//test 1 2 3git 
 let nextScheduleDatas = {
   sort_date: 0,
   vehicleName: "",
