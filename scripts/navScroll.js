161cc1596c0839af5584a846f04c5ac8b3f64bb9
jungwonJung <wjdwjd1501@gmail.com>
2023-05-29 18:38:30 +0200

    hotfix-v5

--- scripts/navScroll.js
+++ scripts/navScroll.js
@@ -1,21 +1,27 @@
+const navLinks = document.querySelectorAll(".nav-buttons a");
 
-const navLinks = document.querySelectorAll('.nav-buttons a');
-
-navLinks.forEach(link => {
-  link.addEventListener('click', function(event) {
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (event) {
     event.preventDefault();
     removeActiveClass();
-    this.classList.add('active-link');
-    const targetId = this.getAttribute('href');
+    this.classList.add("active-link");
+    const targetId = this.getAttribute("href");
     const targetSection = document.querySelector(targetId);
     if (targetSection) {
-      targetSection.scrollIntoView({ behavior: 'smooth' });
+      targetSection.scrollIntoView({ behavior: "smooth" });
     }
   });
 });
 
 function removeActiveClass() {
-  navLinks.forEach(link => {
-    link.classList.remove('active-link');
+  navLinks.forEach((link) => {
+    link.classList.remove("active-link");
   });
 }
+
+const logoImg = document.getElementById("logo-img");
+const navButtons = document.querySelector(".nav-buttons");
+
+logoImg.addEventListener("click", function () {
+  navButtons.classList.toggle("hidden");
+});
