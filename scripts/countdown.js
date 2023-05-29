dedda16e9ed11c1d154a70b10d0654f2b6d8cdf5
jungwonJung <wjdwjd1501@gmail.com>
2023-05-29 15:56:21 +0200

    make module file and delete duplicate code

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,72 +1,40 @@
-const API_URL = "https://fdo.rocketlaunch.live/json/launches/next/5";
+import { getData } from "./getData.js";
+import { displayMap } from "./getLocation.js";
+
 const countdown = document.getElementById("countdown");
 const infoBox = document.getElementById("info-box");
 
-let nextScheduleDatas = {
-  sort_date: 0,
-  vehicleName: "",
-  missonName: "",
-  launchDescription: "",
-  country: "",
-  location: "",
-  state: "",
-};
-
-async function getData() {
-  // rest API get post update delete
-  try {
-    const response = await fetch(API_URL);
-    if (response.status === 200) {
-      const data = await response.json();
-      const findData = data.result.find(
-        (item) => item.provider.name === "SpaceX"
-      );
-      nextScheduleDatas.sort_date = findData.sort_date;
-      nextScheduleDatas.vehicleName = findData.vehicle.name;
-      nextScheduleDatas.missonName = findData.name;
-      nextScheduleDatas.launchDescription = findData.launch_description;
-      nextScheduleDatas.country = findData.pad.location.country;
-      nextScheduleDatas.location = findData.pad.location.name;
-      nextScheduleDatas.state = findData.pad.location.state;
-      return nextScheduleDatas;
-    } else {
-      throw new Error("404");
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 async function countDown() {
-  const {
-    missonName,
-    vehicleName,
-    // country,
-    // state,
-    // location,
-    launchDescription,
-  } = await getData();
+  const { missonName, vehicleName, sort_date, launchDescription } =
+    await getData();
 
   const currentUnixTime = Date.now() / 1000; // get now time with unix time stamp
 
   let comparedTime = Infinity; // compared Time now with next schdule time
 
-  const unixTimestamp = Number(nextScheduleDatas.sort_date); // for example 1600002341
+  const unixTimestamp = Number(sort_date); // for example 1600002341
   const diff = unixTimestamp - currentUnixTime; //  for example unixTimestamp = 24  currentTimeUnix = 21
 
   // try find most closest time schedule
   comparedTime = diff;
 
   if (comparedTime !== Infinity) {
-    const days = String(Math.floor(comparedTime / (24 * 60 * 60))).padStart(2, '0');
-    const hours = String(Math.floor((comparedTime % (24 * 60 * 60)) / (60 * 60))).padStart(2, '0');
-const minutes = String(Math.floor((comparedTime % (60 * 60)) / 60)).padStart(2, '0');
-const seconds = String(Math.floor(comparedTime % 60)).padStart(2, '0');
+    const days = String(Math.floor(comparedTime / (24 * 60 * 60))).padStart(
+      2,
+      "0"
+    );
+    const hours = String(
+      Math.floor((comparedTime % (24 * 60 * 60)) / (60 * 60))
+    ).padStart(2, "0");
+    const minutes = String(
+      Math.floor((comparedTime % (60 * 60)) / 60)
+    ).padStart(2, "0");
+    const seconds = String(Math.floor(comparedTime % 60)).padStart(2, "0");
 
     countdown.innerHTML = `<div class="separateTime"><p class="timeChange">${days}:</p> <p class="times">Day(s)</p></div>
     <div class="separateTime"><p class="timeChange">${hours}:</p> <p class="times">Hour(s)</p></div>
     <div class="separateTime"><p class="timeChange">${minutes}:</p> <p class="times">Minute(s)</p></div>
-    <div class="separateTime"><p class="timeChange">${seconds}</p> <p class="times">Second(s)</p></div>`
+    <div class="separateTime"><p class="timeChange">${seconds}</p> <p class="times">Second(s)</p></div>`;
 
     infoBox.innerHTML = `
       <p><b style="color:#0AC6FF;">Mission:</b> ${missonName}</p><br>
@@ -80,5 +48,6 @@ const seconds = String(Math.floor(comparedTime % 60)).padStart(2, '0');
 
 window.onload = () => {
   countDown();
+  displayMap();
   setInterval(countDown, 1000);
 };
--- scripts/getLocation.js
+++ scripts/getLocation.js
@@ -1,28 +1,27 @@
-const API_BING = "AlwdnW7MMtPdYqpELeL8RZhaTO_Q_sLaonj958GFtex5jqKeHuVWg-n3dinIfBsN";
-let isUpcomingLaunchDisplayed = false;
+import { getData } from "./getData.js";
+const data = await getData();
+
+const API_BING =
+  "AlwdnW7MMtPdYqpELeL8RZhaTO_Q_sLaonj958GFtex5jqKeHuVWg-n3dinIfBsN";
 
 async function getLocation() {
-  
   // fetch basic data for upcoming SpaceX launch from rocketlaunch PPI
 
-  const rocketLaunchPromise = await fetch("https://fdo.rocketlaunch.live/json/launches/next/5");
-  const rocketLaunchData = await rocketLaunchPromise.json();
-  const nextSpaceXLaunch = rocketLaunchData.result.find(launch => launch.provider.name === "SpaceX");
-  const locationOfLaunch = nextSpaceXLaunch.pad.location.name;
-  
+  const locationOfLaunch = data.location;
+  const { state, country } = data;
+
   // Display Rocket Info in div next to map
 
   const mapInfo = document.querySelector(".map-info");
-  const {name, state, country} = nextSpaceXLaunch.pad.location;
   mapInfo.innerHTML = `
     <p>Country: <span class="launch-detail">${country}<br>
     <p>State: <span class="launch-detail">${state}<br>
-    <p>Launch Pad: <span class="launch-detail">${name}<br>`
-  
+    <p>Launch Pad: <span class="launch-detail">${locationOfLaunch}<br>`;
+
   // Determine coordinates from one of four possible facilities where rocket can be launched
-  
+
   let coordinates = [];
-  
+
   if (locationOfLaunch.toLowerCase().includes("vandenberg")) {
     coordinates = [34.74822601041721, -120.57115951808824];
   } else if (locationOfLaunch.toLowerCase().includes("canaveral")) {
@@ -33,39 +32,38 @@ async function getLocation() {
     coordinates = [25.997489955299834, -97.15726721895248];
   }
 
-  const [lon, lat] = coordinates;
-  GetMap(lon, lat, locationOfLaunch)
+  return coordinates;
 }
 
-function GetMap(lon, lat, name) {
-  
-  if(isUpcomingLaunchDisplayed === false) {
-    getLocation();
-    isUpcomingLaunchDisplayed = true;
-    return;
-  }
-   
-    let map = new Microsoft.Maps.Map('#myMap', {
-      credentials: API_BING,
-      mapTypeId: Microsoft.Maps.MapTypeId.canvasDark,
-    supportedMapTypes: [Microsoft.Maps.MapTypeId.road, Microsoft.Maps.MapTypeId.aerial, Microsoft.Maps.MapTypeId.canvasDark],
-      center: new Microsoft.Maps.Location(lon, lat)
+function getMap(lon, lat, name) {
+  let map = new Microsoft.Maps.Map("#myMap", {
+    credentials: API_BING,
+    mapTypeId: Microsoft.Maps.MapTypeId.canvasDark,
+    supportedMapTypes: [
+      Microsoft.Maps.MapTypeId.road,
+      Microsoft.Maps.MapTypeId.aerial,
+      Microsoft.Maps.MapTypeId.canvasDark,
+    ],
+    center: new Microsoft.Maps.Location(lon, lat),
   });
-
-
 
   let center = map.getCenter();
 
   //Create custom Pushpin
   let pin = new Microsoft.Maps.Pushpin(center, {
-      title: name,
-      color: '#0ac6ff'
-      // subTitle: 'City Center',
-      // text: '1'
+    title: name,
+    color: "#0ac6ff",
+    // subTitle: 'City Center',
+    // text: '1'
   });
 
   //Add the pushpin to the map
   map.entities.push(pin);
+}
 
-   }
+export async function displayMap() {
+  const [lon, lat] = await getLocation();
+  getMap(lon, lat, data.location);
+}
 
+export default displayMap;
